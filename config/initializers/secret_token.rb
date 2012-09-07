eda496e1d1e03af02a0f7e8b4bcf6397b4ca7619
Bill Transue <transue@gmail.com>
2012-09-07 18:33:21 -0400

    Create Initial Rails Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LocationSquared::Application.config.secret_token = 'b0d395df110fda204ce314860c8bc56bff11a931714d3645ed3acf018cfbb5c0f07d1b3be967f3a258bf6e84e768907034f28c1ffa58c2a6be9c046ea1ea33dd'
