0eec6fedb5a849e74a84cbc7537d337acaba3949
Bill Transue <transue@gmail.com>
2012-09-09 17:16:09 -0400

    Create sessions#create Action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,13 @@
 class SessionsController < ApplicationController
   def create
-    puts auth = request.env['omniauth.auth']
+    auth = request.env['omniauth.auth']
+    if user = User.find_or_create_by_foursquare_id(auth['uid'])
+      flash[:notice] = "Signed in as #{user.name}."
+      user.update_attributes :name => auth['extra']['raw_info']['firstName'],
+                             :photo => auth['extra']['raw_info']['photo']
+    else
+      flash[:error] = "Couldn't create account..."
+    end
+    redirect_to root_url
   end
 end
