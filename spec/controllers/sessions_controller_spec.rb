2af5ca4bbc51846fef817d9f82e704e71c130f4e
Bill Transue <transue@gmail.com>
2012-09-10 08:41:06 -0400

    Spec Sessions Controller Create Action

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe SessionsController do
+  before :suite do
+    OmniAuth.config.test_mode = true
+  end
 
+  describe "create action" do
+    context "new user" do
+      before :each do
+        OmniAuth.config.mock_auth[:foursquare] = OmniAuth::AuthHash.new {
+          :provider => 'foursquare',
+          :uid => '123545',
+          :extra => {
+            :raw_info => {
+              :firstName => 
+            }
+          }
+        }
+      end
+    end
+  end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,20 @@
 
 FactoryGirl.define do
   factory :user do
+    foursquare_id { rand (10 ** 5) }
+    name { Faker::Name.first_name }
+    photo "http://playfoursquare.s3.amazonaws.com/userpix_thumbs/0QYGOJMDGLTJOOQG.jpg"
+
+    trait :confirmed_email do
+      email { Faker::Internet.email }
+      confirmed_at { Time.now }
+      confirmation_sent_at { 5.minutes.ago }
+    end
+
+    trait :unconfirmed_email do
+      unconfirmed_email { Faker::Internet.email }
+      confirmation_token { rand(10 ** 10) }
+      confirmation_sent_at { 5.minutes.ago }
+    end
   end
 end
